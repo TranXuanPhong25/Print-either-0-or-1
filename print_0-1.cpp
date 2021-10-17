50be5b2c5602f3e3b6fe881c4753d48ea13100f6
TranXuanPhong <tranxuanphong26525@gmail.com>
2021-10-17 21:31:56 +0700

    add comment

--- print_0-1.cpp
+++ print_0-1.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main()
 {
+	//cre:stackoverflow > console turn on fullscreen 
 	system("mode con COLS=700");
 	ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
 	SendMessage(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+	
+	//enlarge the font-size and set font-color to green
 	CONSOLE_FONT_INFOEX cfi;
 	cfi.cbSize = sizeof(cfi);
 	cfi.nFont = 0;
@@ -20,18 +23,26 @@ int main()
 	system("color 2");
 	
 	int count;
-	cout<<"->nhap : ";
+	cout<<"|--->Enter something : ";
 	cin >> count;
-	if(count<=20000){
-		count*=20000;
+	
+	//change the following lines below to custom number of appearances
+	if(count<=20000||count<=0){
+		
+		//if input is string or <=0 , it would add more times 
+		count+=69;
+		count*=23145;
 	}
+	
+	//initialize array which stores 0s and 1s
 	char arrayNum[8] = {'1', '0', '1', '0', '1', '0', '0','1'};
 	for (int i = 0; i < count; i += 2)
 	{
-
-		int RandIndex = rand() % 5; //generates a random number between 0 and 3
+		int RandIndex = rand() % 5; //generates a random number between 0 and 5 ? I don't sure about this
 		cout << arrayNum[RandIndex];
 	}
+	
+	//the end of program
 	cout<<"\n\t\t\t\t\tNASA HACKED";
 	int none ;
 	cin>>none;
